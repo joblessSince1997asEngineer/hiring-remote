6f0ef9818868286c0ba0d0ffbfc15d7299df54e6
Syed Mohammad Abul Quasim <smaqmiz@gmail.com>
2026-08-03 20:06:52 +0530

    Fixed prisma.config.ts provider error

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,6 @@ dotenv.config();
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    provider: 'postgresql',
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL, // Only keep this line!
   },
 });
